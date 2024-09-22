cfe6b31947985dd6d604531f5144317e7c593551
JillRay973 <143532365+JillRay973@users.noreply.github.com>
2024-09-22 11:22:41 -0400

    lambert-vert.glsl

---
+++ src/shaders/lambert-vert.glsl
@@ -0,0 +1,202 @@
+#version 300 es
+
+//This is a vertex shader. While it is called a "shader" due to outdated conventions, this file
+//is used to apply matrix transformations to the arrays of vertex data passed to it.
+//Since this code is run on your GPU, each vertex is transformed simultaneously.
+//If it were run on your CPU, each vertex would have to be processed in a FOR loop, one at a time.
+//This simultaneous transformation allows your program to run much faster, especially when rendering
+//geometry with millions of vertices.
+
+uniform mat4 u_Model;       // The matrix that defines the transformation of the
+                            // object we're rendering. In this assignment,
+                            // this will be the result of traversing your scene graph.
+
+uniform mat4 u_ModelInvTr;  // The inverse transpose of the model matrix.
+                            // This allows us to transform the object's normals properly
+                            // if the object has been non-uniformly scaled.
+
+uniform mat4 u_ViewProj;    // The matrix that defines the camera's transformation.
+                            // We've written a static matrix for you to use for HW2,
+                            // but in HW3 you'll have to generate one yourself
+
+in vec4 vs_Pos;             // The array of vertex positions passed to the shader
+
+in vec4 vs_Nor;             // The array of vertex normals passed to the shader
+
+in vec4 vs_Col;             // The array of vertex colors passed to the shader.
+
+out vec4 fs_Nor;            // The array of normals that has been transformed by u_ModelInvTr. This is implicitly passed to the fragment shader.
+out vec4 fs_LightVec;       // The direction in which our virtual light lies, relative to each vertex. This is implicitly passed to the fragment shader.
+out vec4 fs_Col;            // The color of each vertex. This is implicitly passed to the fragment shader.
+out vec4 fs_Pos;
+
+uniform float u_Time;
+
+//uniform vec4 u_Look;
+
+const vec4 lightPos = vec4(5, 5, 3, 1); //The position of our virtual light, which is used to compute the shading of
+                                        //the geometry in the fragment shader.
+
+float random2(vec2 co){
+    return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
+}
+
+float random3(vec3 co){
+    float x = random2(vec2(co[0], co[1]));
+    float y = random2(vec2(co[1], co[2]));
+    float z = random2(vec2(co[0], co[2]));
+
+    return random2(vec2(random2(vec2(x, y)), random2(vec2(y, z))));
+}
+
+float surflet(vec3 p, vec3 gridPoint) {
+    //Compute the distance between p and the grid point along each axis, and warp it with a
+    //quintic function so we can smooth our cells
+    vec3 t2 = abs(p - gridPoint);
+    vec3 t = vec3(1.f, 1.f, 1.f) - 6.f * vec3(pow(t2[0], 5.f), pow(t2[1], 5.f), pow(t2[2], 5.f)) + 15.f *vec3(pow(t2[0], 4.f), pow(t2[1], 4.f), pow(t2[2], 4.f)) - 10.f * vec3(pow(t2[0], 3.f), pow(t2[1], 3.f), pow(t2[2], 3.f));
+    // Get the random vector for the grid point (assume we wrote a function random2
+    // that returns a vec2 in the range [0, 1])
+    vec3 gradient = random3(gridPoint) * 2. - vec3(0.1f, 0.5f, 1.f);
+    // Get the vector from the grid point to P
+    vec3 diff = p - gridPoint;
+    // Get the value of our height field by dotting grid->P with our gradient
+    float height = dot(diff, gradient);
+    // Scale our height field (i.e. reduce it) by our polynomial falloff function
+    return height * t.x * t.y * t.z;
+}
+
+float perlinNoise3D(vec3 p) {
+	float surfletSum = 0.f;
+	// Iterate over the four integer corners surrounding uv
+	for(int dx = 0; dx <= 1; ++dx) {
+		for(int dy = 0; dy <= 1; ++dy) {
+			for(int dz = 0; dz <= 1; ++dz) {
+				surfletSum += surflet(p, floor(p) + vec3(dx, dy, dz));
+			}
+		}
+	}
+	return surfletSum;
+}
+
+// higher-frequency, lower-amplitude layer of fbm
+float fbm(vec3 p) {
+    float total = 0.f;
+    float persistence = 0.5f;
+    int octaves = 8;
+    float freq = 2.f;
+    float amp = 0.5f;
+    for(int i = 1; i <= octaves; i++) {
+        total += amp;
+
+        total += perlinNoise3D(p * freq) * amp;
+
+        freq *= 2.f;
+        amp *= persistence;
+    }
+    return total;
+}
+
+vec3 hash33(vec3 p3) {
+	vec3 p = fract(p3 * vec3(.1031,.11369,.13787));
+    p += dot(p, p.yxz+19.19);
+    return -1.0 + 2.0 * fract(vec3((p.x + p.y)*p.z, (p.x+p.z)*p.y, (p.y+p.z)*p.x));
+}
+
+float worley(vec3 p, float scale){
+    vec3 id = floor(p*scale);
+    vec3 fd = fract(p*scale);
+
+    float n = 0.;
+
+    float minimalDist = 1.;
+
+    for(float x = -1.; x <=1.; x++){
+        for(float y = -1.; y <=1.; y++){
+            for(float z = -1.; z <=1.; z++){
+
+                vec3 coord = vec3(x,y,z);
+                vec3 rId = hash33(mod(id+coord,scale))*0.5+0.5;
+
+                vec3 r = coord + rId - fd; 
+
+                float d = dot(r,r);
+
+                if(d < minimalDist){
+                    minimalDist = d;
+                }
+
+            }
+        }
+    }
+    return 1.0-minimalDist;
+}
+
+float bias (float b, float t) {
+    return pow(t, log(b) / log(0.5f));
+}
+
+float gain (float g, float t) {
+    if (t < 0.5f) {
+        return bias(1.f - g, 2.f * t) / 2.f;
+    } else {
+        return 1.f - bias(1.f - g, 2.f - 2.f * t) / 2.f;
+    }
+}
+
+float noise(vec3 vec) {
+    float amplitude = 5.0f;
+    float freq = 0.2f;
+    //vec3 offset = vec3(5.f, 5.f, 5.f);
+    float total = amplitude * sin(freq * bias(sin(worley(vec.xyz, 5.f) + 0.5f), 0.3f)) ;//amplitude * sin(freq * input.x);
+    //total *= amplitude * sin(freq * fbm(vec));
+    return total;
+}
+
+void main()
+{
+    fs_Col = vs_Col;                         // Pass the vertex colors to the fragment shader for interpolation
+    fs_Pos = vs_Pos;
+
+    float noisyValue = noise(fs_Pos.xyz * sin(u_Time / 1000.f));
+   
+    // Making the flame stretch, with a tail at the top
+    if (fs_Pos.y < 0.f) {
+        fs_Pos.y *= 1.3f;
+    }
+    if (fs_Pos.y < -0.7f) {
+        fs_Pos.y *= bias(abs(fs_Pos.y) + 0.1f * noisyValue, 0.2f);
+    }
+  
+
+    fs_Pos.x *= bias(noise(fs_Pos.xyz * sin(u_Time / 1000.f)), 0.8f);
+    fs_Pos.y *= bias(noise(fs_Pos.xyz * sin(u_Time / 1000.f)), 0.8f);
+    fs_Pos.z *= bias(noise(fs_Pos.xyz * sin(u_Time / 1000.f)), 0.8f);
+
+    // second layer of finer surface level detail?
+    fs_Pos.x *= sin(5.f * fbm(fs_Pos.xyz)); // + bias(sin(0.1f * fbm(fs_Pos.xyz)), 0.1f);
+    fs_Pos.y *= sin(5.f * fbm(fs_Pos.xyz)); // * bias(2.0f * fs_Pos.y, 0.9f) *
+    fs_Pos.z *= sin(5.f * fbm(fs_Pos.xyz));
+
+
+     fs_Pos.y *= worley(fs_Pos.xyz, 0.4f * sin(u_Time / 3000.f));;
+    fs_Pos.z *= worley(fs_Pos.xyz, 0.4f * sin(u_Time / 3000.f));;
+
+  
+
+
+
+    mat3 invTranspose = mat3(u_ModelInvTr);
+    fs_Nor = vec4(invTranspose * vec3(vs_Nor), 0);          // Pass the vertex normals to the fragment shader for interpolation.
+                                                            // Transform the geometry's normals by the inverse transpose of the
+                                                            // model matrix. This is necessary to ensure the normals remain
+                                                            // perpendicular to the surface after the surface is transformed by
+                                                            // the model matrix.
+
+
+    vec4 modelposition = u_Model * fs_Pos;   // Temporarily store the transformed vertex positions for use below
+
+    fs_LightVec = lightPos - modelposition - vec4(10.f, 50.f, 5.f, 1.f);  // Compute the direction in which the light source lies
+
+    gl_Position = u_ViewProj * modelposition;// gl_Position is a built-in variable of OpenGL which is
+                                             // used to render the final positions of the geometry's vertices
+}
